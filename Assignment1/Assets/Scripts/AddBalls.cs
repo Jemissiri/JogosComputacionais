979c4d454f2dacc9978e1f63d99d0934ea743d82
Jemissiri <tgranja4@gmail.com>
2026-02-10 14:18:26 +0000

    first assignment done

--- Assignment1/Assets/Scripts/AddBalls.cs
+++ Assignment1/Assets/Scripts/AddBalls.cs
@@ -5,21 +5,23 @@ public class AddBalls : MonoBehaviour
     public GameObject ball;
     void Start()
     {
-        InvokeRepeating("addBall", 3, 5);
+        InvokeRepeating("AddBall", 3, 5);
     }
 
-    void addBall()
+    void AddBall()
     {
         const int xmin = -3;
         const int xmax = 3;
         const int ymin = 20;
         const int ymax = 30;
-        const int zmin = 100;
+        const int zmin = 145;
         const int zmax = 195;
         float x = Random.Range(xmin, xmax);
         float y = Random.Range(ymin, ymax);
         float z = Random.Range(zmin, zmax);
         Vector3 position = new Vector3(x, y, z);
+
+        Debug.Log(position);
 
         Instantiate(ball, position, Quaternion.identity);
     }
