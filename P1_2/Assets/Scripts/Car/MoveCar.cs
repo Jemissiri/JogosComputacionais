485db58884dda3f5e776ac7a55d6399bd8c3ffc6
FeriDS <diogo1silva1@gmail.com>
2026-03-05 18:19:20 +0000

    NPC spawning done + Despawning

--- P1_2/Assets/Scripts/Car/MoveCar.cs
+++ P1_2/Assets/Scripts/Car/MoveCar.cs
@@ -4,13 +4,14 @@ public class MoveCar : MonoBehaviour
 {
     public float maxSpeed = 5f;
     public float fuel = 100;
-    [SerializeField] private Driver driver;
+    [SerializeField] public Driver driver;
 
     private void Update()
     {
         if (fuel > 0)
         {
             Vector3 dPosition = driver.move(maxSpeed);
+            dPosition.x = dPosition.z;
             dPosition.z *= Mathf.Cos(transform.eulerAngles.y * Mathf.Deg2Rad);
             dPosition.x *= Mathf.Sin(transform.eulerAngles.y * Mathf.Deg2Rad);
             if (dPosition.z * dPosition.z > 0)
--- P1_2/Assets/Scripts/Spawner.cs
+++ P1_2/Assets/Scripts/Spawner.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject ball;
-    public float SpawnRate = 5f;
+    public GameObject[] vehicles;
+    public float BallSpawnRate = 5f;
+    public float VehicleTopLeftSpawnRate = 5f;
+    public float VehicleBottomRightSpawnRate = 10f;
 
     void Start()
     {
-        InvokeRepeating("AddBall", 3, SpawnRate);
-        InvokeRepeating("AddBall", 3, SpawnRate);
+        InvokeRepeating("AddBall", 3, BallSpawnRate);
+        InvokeRepeating("AddTopLeftVehicle", 3, VehicleTopLeftSpawnRate);
+        InvokeRepeating("AddBottomRightVehicle", 3, VehicleBottomRightSpawnRate);
     }
 
     // Update is called once per frame
@@ -17,13 +21,43 @@ public class Spawner : MonoBehaviour
         
     }
 
+    void AddBall()
+    {
+        const int xmin = -3;
+        const int xmax = 4;
+        const int ymin = 15;
+        const int ymax = 25;
+        const int zmin = 95;
+        const int zmax = 195;
+        float x = Random.Range(xmin, xmax);
+        float y = Random.Range(ymin, ymax);
+        float z = Random.Range(zmin, zmax);
+        Vector3 position = new Vector3(x, y, z);
+
+        Debug.Log(position);
+
+        Instantiate(ball, position, Quaternion.identity);
+    }
+    
+    void addVehicles(Vector3 pos, float speed, Quaternion rot) 
+    {
+        int v = (int) Mathf.Round(Random.Range(0,vehicles.Length));
+        GameObject vehicle = Instantiate(vehicles[v], pos, rot);
+        vehicle.AddComponent<Despawner>();
+        AI ai = vehicle.AddComponent<AI>();
+        MoveCar mc = vehicle.AddComponent<MoveCar>();
+        mc.fuel = 100;
+        mc.maxSpeed = speed;
+        mc.driver = ai;
+    }
+
     void AddTopLeftVehicle()
     {
-        
+        addVehicles(new Vector3(-1.85f, 10, 180), 3f, Quaternion.Euler(0, 180, 0));
     }
 
     void AddBottomRightVehicle()
     {
-        
+        addVehicles(new Vector3(1.85f, 2, 5), 3f, Quaternion.Euler(0, 0, 0));
     }
 }
