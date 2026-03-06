e1b650978bcb10d704efe0be482701aeb94d24b5
Jemissiri <tgranja4@gmail.com>
2026-03-06 16:46:56 +0000

    hit counter done

--- P1_2/Assets/Scripts/Car/Human.cs
+++ P1_2/Assets/Scripts/Car/Human.cs
@@ -5,6 +5,8 @@ using UnityEngine.UIElements;
 class Human : Driver
 {
     private Vector2 _deltaMove;
+    private int hits;
+
     public override Vector3 move(float maxSpeed) {
         //move forward
         float dz = _deltaMove.y;
@@ -12,6 +14,7 @@ class Human : Driver
         rotate(dx);
         return new Vector3(dz, 0, dz) * maxSpeed;
     }
+
     public void OnMove(InputValue value) {
         _deltaMove = value.Get<Vector2>();
     }
@@ -20,4 +23,23 @@ class Human : Driver
         if (Mathf.Approximately(dz, 0f)) return;
         transform.Rotate(0f, Mathf.Sign(dz), 0f, Space.Self);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform root = collision.transform.root;
+
+        bool collidedWithVehicle = root.GetComponent<MoveCar>() != null;
+        bool collidedWithBall = root.name.StartsWith("Ball");
+
+        if (collidedWithVehicle || collidedWithBall)
+        {
+            hits++;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "hits: " + hits);
+    }
+
 }
