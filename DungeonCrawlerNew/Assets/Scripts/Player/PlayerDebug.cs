816de61bfab02a7a26d2d29bb81f4c77b0ef45af
Jemissiri <tgranja4@gmail.com>
2026-06-14 17:48:48 +0100

    room 2 started

--- DungeonCrawlerNew/Assets/Scripts/Player/PlayerDebug.cs
+++ DungeonCrawlerNew/Assets/Scripts/Player/PlayerDebug.cs
@@ -3,7 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerDebug : MonoBehaviour
 {
-    [SerializeField] private Vector3 resetPosition = new Vector3(0f, 1f, 0f);
+    [SerializeField] private Vector3 resetPosition1 = new Vector3(0f, 1f, 0f);
+    [SerializeField] private Vector3 resetPosition2 = new Vector3(0f, 1f, 0f);
 
     private CharacterController _cc;
 
@@ -15,20 +16,36 @@ public class PlayerDebug : MonoBehaviour
     private void Update()
     {
         if (Keyboard.current.pKey.wasPressedThisFrame)
-            ResetPosition();
+            ResetPosition1();
+        if (Keyboard.current.oKey.wasPressedThisFrame)
+            ResetPosition2();
     }
 
-    private void ResetPosition()
+    private void ResetPosition1()
     {
         if (_cc != null)
         {
             _cc.enabled = false;
-            transform.position = resetPosition;
+            transform.position = resetPosition1;
             _cc.enabled = true;
         }
         else
         {
-            transform.position = resetPosition;
+            transform.position = resetPosition1;
+        }
+    }
+
+    private void ResetPosition2()
+    {
+        if (_cc != null)
+        {
+            _cc.enabled = false;
+            transform.position = resetPosition2;
+            _cc.enabled = true;
+        }
+        else
+        {
+            transform.position = resetPosition2;
         }
     }
 }
